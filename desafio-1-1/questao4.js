6228b4d8ddf4c41e9b8f73b3736c09c0e175f9b6
Victor Valentim <jvvalentim.096@gmail.com>
2024-10-31 03:44:46 -0300

    questao4

---
+++ desafio-1-1/questao4.js
@@ -0,0 +1,21 @@
+import { Turma } from "./models/Turma.js";
+
+const main = () => {
+    const turma = new Turma();
+
+    turma.inserirAluno("23456", "Bruno Carvalho");
+    turma.inserirAluno("45678", "Joao Santos");
+    turma.inserirAluno("34567", "Fernanda Abreu");
+    turma.inserirAluno("12345", "Ana de Almeida");
+
+    turma.lancarNotaP1("12345", 8.0);
+    turma.lancarNotaP2("12345", 9.5);
+
+    turma.lancarNotaP1("23456", 7.0);
+
+    turma.lancarNotaP2("34567", 8.5);
+
+    turma.imprimirBoletim();
+};
+
+main();
